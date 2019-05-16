5d646837e970954c2e6b055ae2e2ff7e381a3046
tamnv123 <49956660+tamnv123@users.noreply.github.com>
2019-05-16 18:07:39 +0700

    funciton binh phuong

---
+++ function/js/script.js
@@ -0,0 +1,13 @@
+function binhphuong(x){
+	return x*x;
+}
+var tinhbp=function(x){
+	alert ("ket qua la" +""+(x*x));
+}
+function theky(x){
+	return x/100;
+   
+}
+var z=function(x){
+	alert("kq"+ math.ceil(x/100));
+}
